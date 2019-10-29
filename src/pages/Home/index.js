ed3dd76740d41e77f43efa8c4d4a14b70d0b29c8
shaka0241 <shaka0241@gmail.com>
2019-10-29 15:27:12 -0300

    usando primer hook

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,38 @@
-import React from 'react';
-import {Container, Typography} from '@material-ui/core'
+import React, { useState } from 'react';
+import {Container, Typography, Card, Grid, TextField, Button} from '@material-ui/core'
 
-export default () =>(
-	<Container><Typography variant="h3">Home</Typography></Container>
+export default () =>{
+	const [searchText, setSearchText] = useState('');
+	const handleSearchTextChange = event => {
+		setSearchText(event.target.value);
+	};
+	const handleCleanTextClick = event => {
+		console.log('10');
+	};
+	const handleSearchTextClick = event => {
+		console.log('10');
+	};
+	return(
+		<Container>
+		<Card>
+			<Grid container>
+				<Grid>
+					<Typography> Bienvenido! </Typography>
+				</Grid>
+				<Grid>
+					<label>
+					Icono
+					</label>
+				</Grid>
+			</Grid>
+			<TextField value={searchText}
+			placeholder="Buscar..."
+			onChange={handleSearchTextChange}/>
+			<Grid>
+				<Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button> 
+				<Button variant="contained" color="primary" size="large" onClick={handleSearchTextClick}>Buscar</Button>
+			</Grid>
+		</Card>
+	</Container>
 	)
+}
