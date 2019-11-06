2a437fc4b2aaf09f4418f8e8bc81ecd50946ac47
shaka0241 <shaka0241@gmail.com>
2019-11-06 17:58:58 -0300

    use dispatch result page

--- src/pages/MovieDetail/index.js
+++ src/pages/MovieDetail/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { searchMovieById, searchMovie } from '../../redux/actions/search';
 
-export default () => {
+export default ( { match }) => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const movieId =  match.params.id
+        dispatch(searchMovieById({movieId}));
+    });
+    
     return (
         <Container>
             MovieDetail
