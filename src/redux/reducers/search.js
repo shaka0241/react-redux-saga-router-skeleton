5264b9c4dc8e882d470753c6d1368adbfacbfa20
shaka0241 <shaka0241@gmail.com>
2019-11-06 17:45:07 -0300

    byid reducers

--- src/redux/reducers/search.js
+++ src/redux/reducers/search.js
@@ -1,5 +1,10 @@
 import { get } from 'lodash';
-import { SEARCH_MOVIE_START, SEARCH_MOVIE_ERROR, SEARCH_MOVIE_COMPLETE } from '../../consts/actionTypes';
+import { SEARCH_MOVIE_START,
+    SEARCH_MOVIE_ERROR, 
+    SEARCH_MOVIE_COMPLETE, 
+    SEARCH_MOVIE_BY_ID_COMPLETE, 
+    SEARCH_MOVIE_BY_ID_ERROR, 
+    SEARCH_MOVIE_BY_ID_START } from '../../consts/actionTypes';
 
 const initialState = {};
 
@@ -10,12 +15,25 @@ export default function (state = initialState, action) {
             break;
 
         case SEARCH_MOVIE_ERROR:
-            console.log(action);
-            return{ ...state, isLoading: false, movies: null };
+            return{ ...state, isLoading: false, movieResults: null  };
             break;
 
         case SEARCH_MOVIE_COMPLETE:
             return{ ...state, isLoading: false, movieResults: action.results.data };
+            break;
+
+            case SEARCH_MOVIE_BY_ID_START:
+            return{ ...state, isLoading: true, movieResult: null };
+            break;
+
+        case SEARCH_MOVIE_BY_ID_ERROR:
+            console.log(action);
+            return{ ...state, isLoading: false, movieResult: null };
+            break;
+
+        case SEARCH_MOVIE_BY_ID_COMPLETE:
+            console.log(action);
+            return{ ...state, isLoading: false, movieResult: action.movie.data };
             break;
     
         default:
